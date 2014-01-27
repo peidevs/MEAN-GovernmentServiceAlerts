45eca9e7e277af33554800f225af39d53c7f3976
alar <arothmel@gmail.com>
2014-01-27 16:27:08 -0400

    Fix app definition

--- app.js
+++ app.js
@@ -5,9 +5,12 @@
    $ node app cloud9  
 */
 
-var config = require('./config')()
-  , app = require('express')
-  , http = require('http');
+var express = require('express')
+  , config = require('./config')()
+  , http = require('http')
+  , MongoClient = require('mongodb').MongoClient;
+
+var app = express();
 
 http.createServer(app).listen(config.port, function(){
     console.log('Express server listening on port ' + config.port);
