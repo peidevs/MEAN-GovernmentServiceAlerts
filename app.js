2d4964337711ceda7432f66d36f83dc3ac58333d
alar <arothmel@gmail.com>
2014-01-27 16:29:26 -0400

    Add test for db connection

--- app.js
+++ app.js
@@ -12,6 +12,21 @@ var express = require('express')
 
 var app = express();
 
-http.createServer(app).listen(config.port, function(){
-    console.log('Express server listening on port ' + config.port);
+/* Ensure connections available for mongdb and express server */
+MongoClient.connect('mongodb://' + config.mongo.host + ':' + config.mongo.port + '/MEAN-GovernmentServiceAlerts', function(err, db) {
+	if(err) {
+		console.log('Sorry, there is no mongo db server running.');
+	} else {
+		var attachDB = function(req, res, next) {
+			req.db = db;
+			next();
+		};
+
+		http.createServer(app).listen(config.port, function() {
+		  	console.log(
+		  		'Successfully connected to mongodb://' + config.mongo.host + ':' + config.mongo.port,
+		  		'\nExpress server listening on port ' + config.port
+		  	);
+		});
+	}
 });
