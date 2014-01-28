1ed15a489bcfddfc8ca0e141bc64da05c78ffc9a
alar <arothmel@gmail.com>
2014-01-27 21:52:24 -0400

    Remove db connection test

--- app.js
+++ app.js
@@ -12,21 +12,3 @@ var express = require('express')
 
 var app = express();
 
-/* Ensure connections available for mongdb and express server */
-MongoClient.connect('mongodb://' + config.mongo.host + ':' + config.mongo.port + '/MEAN-GovernmentServiceAlerts', function(err, db) {
-	if(err) {
-		console.log('Sorry, there is no mongo db server running.');
-	} else {
-		var attachDB = function(req, res, next) {
-			req.db = db;
-			next();
-		};
-
-		http.createServer(app).listen(config.port, function() {
-		  	console.log(
-		  		'Successfully connected to mongodb://' + config.mongo.host + ':' + config.mongo.port,
-		  		'\nExpress server listening on port ' + config.port
-		  	);
-		});
-	}
-});
