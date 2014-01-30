40d4245cea8c8c089367979b4dcc5709980e886a
alar <arothmel@gmail.com>
2014-01-30 12:04:55 -0400

    Add remoteDB

--- config/index.js
+++ config/index.js
@@ -1,14 +1,33 @@
 var config = {
 	local : {
 		mode: 'local',
-		port: 8885
+		port: 8885,
+		mongo: {
+			host: '127.0.0.1',
+			port: 27017
+		}
 	},
 	cloud9 : {
 		mode: 'cloud9',
-		port: process.env.PORT
+		port: process.env.PORT,
+		mongo: {
+			host: '127.0.0.1',
+			port: 10046
+		}
+	},
+	remoteDB : {
+		mode: 'remoteDB',
+		port: 8885,
+		mongo: {
+			host: 'troup.mongohq.com',
+			port: 10046 ,
+			user: 'scott',
+			pass: 'tiger'
+		}
 	}
 };
 
 module.exports = function(mode) {
 	return config[mode || process.argv[2] || 'local'] || config.local;
 };
+
