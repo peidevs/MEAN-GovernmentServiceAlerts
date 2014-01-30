ad46f2c4292c9e0b33d95d7160343d1e55ae69ac
alar <arothmel@gmail.com>
2014-01-30 12:03:41 -0400

    Add connect to remoteDB

--- app.js
+++ app.js
@@ -1,8 +1,5 @@
-
 /*  Launch as:
-   $ node app local
-   or
-   $ node app cloud9  
+   $ node app (local || cloud9 || remoteDB)
 */
 
 var express = require('express')
@@ -12,6 +9,26 @@ var express = require('express')
 
 var app = express();
 
-http.createServer(app).listen(config.port, function(){
-  console.log('Express server listening on port ' + config.port);
+/* Ensure connections available for mongdb and express server */
+/* We are just attaching here. No logon required... */
+MongoClient.connect('mongodb://' + config.mongo.host + ':' + config.mongo.port + '/services', function(err, db) {
+  if(err) {
+    console.log('Sorry, there is no mongo db server running.');
+  } else {
+    var attachDB = function(req, res, next) {
+      req.db = db;
+      next();
+    };
+
+    http.createServer(app).listen(config.port, function() {
+        console.log(
+          'Successfully connected to mongodb://' + config.mongo.host + ':' + config.mongo.port,
+          '\nExpress server listening on port ' + config.port
+        );
+    });
+  }
 });
+
+
+
+
