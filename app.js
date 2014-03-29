cb961c557670f7b320d6486eceec11e551a65150
alar <arothmel@gmail.com>
2014-03-29 19:02:22 -0300

    Add query by services. Update routes.

--- app.js
+++ app.js
@@ -20,38 +20,25 @@ var app = express();
 app.use(require('stylus').middleware(path.join(__dirname, 'public/dist')));
 app.use(express.static(path.join(__dirname, 'public/dist')));
 
-/* Routes */
-
-
+// Config
 app.get('/profile/new', routes.profileNew);
 app.get('/profile', routes.displayAllProfiles);
 app.get('/profile/:id', routes.displayProfile);
 app.get('/profile/:id/edit', routes.profileEdit);
 app.post('/profile/:id/edit', routes.profileUpdate);
 
+app.get('/service/garbage', routes.serviceGarbage);
+app.get('/service/garbage/:id', routes.serviceGarbageByDistrict);
+app.get('/service/events', routes.serviceEvents);
+app.get('/service/streets', routes.serviceStreetClosures);
 
-/* Ensure connections available for mongdb and express server */
-/* We are just attaching here. No logon required... */
-//MongoClient.connect('mongodb://' + config.mongo.username + ':' + config.mongo.password + '@' +  config.mongo.host + ':' + config.mongo.port + config.mongo.db, function(err, db) {
-MongoClient.connect('mongodb://' + config.mongo.host + ':' + config.mongo.port + '/user', function(err, db) {
 
-  if(err) {
-    console.log('Sorry, there is no mongo db server running.');
-	console.log(err);
-  } else {
-    var attachDB = function(req, res, next) {
-      req.db = db;
-      next();
-    };
 
-    http.createServer(app).listen(config.port, function() {
-        console.log(
-          'Successfully connected to mongodb://' + config.mongo.host + ':' + config.mongo.port,
-          '\nExpress server listening on port ' + config.port
-        );
-    });
-  }
+http.createServer(app).listen(config.port, function() {
+   console.log('Express server listening on port ' + config.port
+   );
 });
+
 
 
 
--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,9 @@
 var ContactProfile = require('../contactprofile').ContactProfile;
 var contactData = new ContactProfile('localhost', 27017);
 
+var ServiceProfile = require('../serviceprofile').ServiceProfile;
+var serviceData = new ServiceProfile('localhost', 27017);
+
 /* exports.index = function(req, res){ // exports is like expose
   res.render('index', { title: 'MEAN - Government Service Alerts' });
 };
@@ -26,6 +29,7 @@ exports.displayProfile = function(req, res) {
 };
 
 
+
 exports.profileEdit = function(req, res){
     contactData.findById(req.params.id, function(error, profiles) {
         res.json(profiles); // Need to break this out into actual fields for edit
@@ -43,3 +47,37 @@ exports.displayAllProfiles = function(req, res) {
     });
 };
 
+// Query find({"service.garbage":1})
+exports.serviceGarbage = function(req, res) {
+    serviceData.findGarbage(function(error, profiles) {
+        res.json(profiles);
+    });
+};
+
+// Query find({"service.garbage":1})
+exports.serviceGarbageByDistrict = function(req, res) {
+    serviceData.findGarbageByDistrict(function(error, profiles) {
+        res.json(profiles);
+    });
+};
+
+// Query find({"address.province":"PEI"})
+exports.serviceProvince = function(req, res) {
+    serviceData.findProvince(function(error, profiles) {
+        res.json(profiles);
+    });
+};
+
+// Query find({"service.garbage":1})
+exports.serviceStreetClosures = function(req, res) {
+    serviceData.findStreetClosures(function(error, profiles) {
+        res.json(profiles);
+    });
+};
+
+// Query find({"service.events":1})
+exports.serviceEvents = function(req, res) {
+    serviceData.findEvents(function(error, profiles) {
+        res.json(profiles);
+    });
+};
