049c45ec83946c9bd080827d3bf440726be091e0
Evan Porter <evan.porter@dotman.ca>
2014-04-10 16:26:51 -0300

    Added silly drop down for fake login

--- public/scripts/controllers/login.js
+++ public/scripts/controllers/login.js
@@ -1,10 +1,21 @@
 'use strict';
 
 angular.module('govServeApp')
-  .controller('LoginCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  });
+  .controller('LoginCtrl', ['$scope', '$location', '$http', 
+      function ($scope, $location, $http) {
+          // On load, we'll query for all the users and populate the drop-down
+          // This is a bad idea, but until we decide how login should work, we'll
+          // throw caution to the wind
+          var promise = $http({ method: 'GET', url: '/profile/' }).
+            success(function(data, status, headers, config) {
+                return data;
+            }).
+            error(function(data, status, headers, config) {
+                alert(data);
+            });
+
+            promise.then(function(data) {
+                $scope.userList = data.data;
+                $scope.selectedUser = data.data[0];
+            });
+      }]);
