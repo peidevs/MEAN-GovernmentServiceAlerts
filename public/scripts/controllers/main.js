d84b1f63be162b055a9e4929c19531ca13381a65
Evan Porter <evan.porter@dotman.ca>
2014-04-13 13:57:00 -0300

    Removing useless comment

--- public/scripts/controllers/main.js
+++ public/scripts/controllers/main.js
@@ -3,7 +3,6 @@
 angular.module('govServeApp')
   .controller('MainCtrl', ['$scope', '$location', function ($scope, $location) {
       $scope.login = function() {
-          //Do something
           $location.path('/login/');
       };
   }]);
